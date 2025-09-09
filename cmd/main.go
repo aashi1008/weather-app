370009b929c65b18aa7aadd67e93b9e053cfa9ca
aashi1008 <156366158+aashi1008@users.noreply.github.com>
2025-09-09 12:04:01 +0200

    clean main.go

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/aashi1008/weather-app/config"
 	"github.com/aashi1008/weather-app/internal/handler"
@@ -18,21 +17,9 @@ func main() {
 	weatherService := service.NewWeatherService(appConfig)
 	weatherHandler := handler.NewWeatherHandler(weatherService)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Connected to server"))
-	})
 	http.HandleFunc("/weather", weatherHandler.GetWeatherHandler)
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("defaulting to port %s", port)
-	}
-
-	// Start HTTP server.
-	log.Printf("listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
+	if err := http.ListenAndServe(":"+appConfig.Port, nil); err != nil {
 		log.Fatal(err)
 	}
-	//log.Fatal(http.ListenAndServe(":8080", nil))
 }
